2655ec036690db32a1060dca20b55307381be852
Upamanyu Sengupta <senguptaupamanyu31@gmail.com>
2026-03-28 20:39:17 +0530

    Delete qrisc2 directory

--- qrisc2/tests/test_mapper.py
+++
@@ -1,8 +0,0 @@
-from compiler.mapper import RegisterMap 
-
-rm = RegisterMap()
-
-print(rm)
-print("x1 →", rm.get_reg_qubits("x1"))
-print("Ancilla →", rm.get_ancilla())
-print("Total qubits →", rm.total_qubits())
